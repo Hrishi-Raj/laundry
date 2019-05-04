87a8ce486afb9454da2f6f2ba922c16f4e3200f5
Acer <43838095+Hrishi-Raj@users.noreply.github.com>
2019-05-04 22:57:16 +0530

    Add files via upload

---
+++ application/views/admin/footer.php
@@ -0,0 +1,42 @@
+<div class="footer">
+	<div class="footer-inner">
+		<div class="footer-content">
+			<span class="bigger-120">
+				<span class="blue bolder"> Laundry Management Application
+			</span>
+
+			
+			
+		</div>
+	</div>
+</div>
+
+<script type="text/javascript">
+			jQuery(function($) {
+				setTimeout(function() {
+					$($('.tableTools-container')).find('a.dt-button').each(function() {
+						var div = $(this).find(' > div').first();
+						if(div.length == 1) div.tooltip({container: 'body', title: div.parent().text()});
+						else $(this).tooltip({container: 'body', title: $(this).text()});
+					});
+				}, 500);
+				
+				$('[data-rel="tooltip"]').tooltip({placement: tooltip_placement});
+				
+				//tooltip placement on right or left
+				function tooltip_placement(context, source) {
+					var $source = $(source);
+					var $parent = $source.closest('table')
+					var off1 = $parent.offset();
+					var w1 = $parent.width();
+			
+					var off2 = $source.offset();
+					//var w2 = $source.width();
+			
+					if( parseInt(off2.left) < parseInt(off1.left) + parseInt(w1 / 2) ) return 'right';
+					return 'left';
+				}
+								
+				
+</script>
+			
